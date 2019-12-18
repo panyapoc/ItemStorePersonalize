3f18c75f40699ed9abc5434647c63928d929ef8a
Chuenwatanakul <panyapoc@f018982d6532.ant.amazon.com>
2019-12-18 13:22:48 +0800

    added personlize search

--- functions/APIs/Search/index.py
+++ functions/APIs/Search/index.py
@@ -5,9 +5,15 @@ import requests
 from requests_aws4auth import AWS4Auth
 
 region = os.environ["REGION"]
+RANKING_ARN = os.environ["RANKING_ARN"]
 service = "es"
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+client = boto3.client('personalize-runtime')
+
+dynamodb = boto3.resource('dynamodb')
+ddb_tablename = os.environ["ddb_tablename"]
+table = dynamodb.Table(ddb_tablename)
 
 index = "items_vanilla"
 type = "items"
@@ -15,17 +21,6 @@ url = "https://" + os.environ["ESENDPOINT"] + "/_search" # ElasticSearch cluster
 
 # Search - Search for books across book names, authors, and categories
 def handler(event, context):
-
-    # Put the user query into the query DSL for more accurate search results.
-    # query = {
-    #     "size": 25,
-    #     "query": {
-    #         "multi_match": {
-    #             "query": event["queryStringParameters"]["q"],
-    #             "fields": ["asin", "title"]
-    #         }
-    #     }
-    # }
 
     query = {
         "size": 25,
@@ -53,18 +48,56 @@ def handler(event, context):
     print(r.text)
     document = json.loads(r.text)
 
-    result = []
+    qresult = []
+    qresultid = []
     for item in document['hits']['hits'] :
-        result.append(item['_source'])
+        qresult.append(item['_source'])
+        qresultid.append(item['_source']['asin'])
 
-    # Create the response and add some extra content to support CORS
-    response = {
-        "statusCode": r.status_code,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": True
-        },
-        "body": json.dumps(result)
-    }
+    try :
+        userId = event["queryStringParameters"]["u"]
+    except :
+        userId = None
+
+    if userId is None:
+        response = {
+            "statusCode": r.status_code,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": True
+            },
+            "body": json.dumps(qresult)
+        }
+    else :
+        rerank = client.get_personalized_ranking(
+            campaignArn=RANKING_ARN,
+            inputList=qresultid,
+            userId=event["queryStringParameters"]["u"]
+        )
+
+        itemlist = [];
+        errcount = 0
+        for item in rerank['personalizedRanking']:
+            itemobj = table.get_item(
+                Key={
+                    'asin': item['itemId']
+                }
+            )
+            try :
+                itemlist.append(itemobj['Item'])
+            except :
+                errcount = errcount+1
+
+        print("Can't find ",errcount)
+
+
+        response = {
+            "statusCode": r.status_code,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": True
+            },
+            "body": json.dumps(itemlist)
+        }
 
     return response
