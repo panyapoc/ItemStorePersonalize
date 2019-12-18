e6104f237b049ab20146e2eaa6653ef87ccd2877
Chuenwatanakul <panyapoc@f018982d6532.ant.amazon.com>
2019-12-18 16:08:35 +0800

    Clear Repo

--- functions/APIs/Search/index.py
+++ functions/APIs/Search/index.py
@@ -5,15 +5,9 @@ import requests
 from requests_aws4auth import AWS4Auth
 
 region = os.environ["REGION"]
-RANKING_ARN = os.environ["RANKING_ARN"]
 service = "es"
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
-client = boto3.client('personalize-runtime')
-
-dynamodb = boto3.resource('dynamodb')
-ddb_tablename = os.environ["ddb_tablename"]
-table = dynamodb.Table(ddb_tablename)
 
 index = "items_vanilla"
 type = "items"
@@ -21,6 +15,17 @@ url = "https://" + os.environ["ESENDPOINT"] + "/_search" # ElasticSearch cluster
 
 # Search - Search for books across book names, authors, and categories
 def handler(event, context):
+
+    # Put the user query into the query DSL for more accurate search results.
+    # query = {
+    #     "size": 25,
+    #     "query": {
+    #         "multi_match": {
+    #             "query": event["queryStringParameters"]["q"],
+    #             "fields": ["asin", "title"]
+    #         }
+    #     }
+    # }
 
     query = {
         "size": 25,
@@ -48,56 +53,20 @@ def handler(event, context):
     print(r.text)
     document = json.loads(r.text)
 
-    qresult = []
-    qresultid = []
+    result = []
     for item in document['hits']['hits'] :
-        qresult.append(item['_source'])
-        qresultid.append(item['_source']['asin'])
+        result.append(item['_source']['asin'])
 
-    try :
-        userId = event["queryStringParameters"]["u"]
-    except :
-        userId = None
-
-    if userId is None:
-        response = {
-            "statusCode": r.status_code,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": True
-            },
-            "body": json.dumps(qresult)
+    # Create the response and add some extra content to support CORS
+    response = {
+        "statusCode": r.status_code,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True
+        },
+        "body": {
+            "result" : result
         }
-    else :
-        rerank = client.get_personalized_ranking(
-            campaignArn=RANKING_ARN,
-            inputList=qresultid,
-            userId=event["queryStringParameters"]["u"]
-        )
-
-        itemlist = [];
-        errcount = 0
-        for item in rerank['personalizedRanking']:
-            itemobj = table.get_item(
-                Key={
-                    'asin': item['itemId']
-                }
-            )
-            try :
-                itemlist.append(itemobj['Item'])
-            except :
-                errcount = errcount+1
-
-        print("Can't find ",errcount)
-
-
-        response = {
-            "statusCode": r.status_code,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": True
-            },
-            "body": json.dumps(itemlist)
-        }
+    }
 
     return response
---
+++ functions/APIs/SearchRerank/index.py
@@ -0,0 +1,65 @@
+import json
+import boto3
+import os
+
+personalize_runtime = boto3.client('personalize-runtime')
+lambdafunction = boto3.client('lambda')
+dynamodb = boto3.resource('dynamodb')
+
+RANKING_ARN = os.environ["RANKING_ARN"]
+SEARCH_ARN = os.environ["SEARCH_ARN"]
+table = dynamodb.Table(os.environ["DDB_TABLE"])
+
+def handler(event, context):
+    sresult = lambdafunction.invoke(
+        FunctionName=SEARCH_ARN,
+        LogType='Tail',
+        Payload=json.dumps(event),
+    )
+    res_json = json.loads(sresult['Payload'].read().decode("utf-8"))
+    res_list = res_json['body']['result']
+
+    print("Before list: ",res_list)
+
+    try :
+        userId = event["queryStringParameters"]["u"]
+    except :
+        userId = None
+
+    if userId != None :
+        rerank = personalize_runtime.get_personalized_ranking(
+            campaignArn=RANKING_ARN,
+            inputList=res_list,
+            userId=userId
+        )
+        res_list = []
+
+        # print(rerank)
+        for item in rerank['personalizedRanking'] :
+            res_list.append(item['itemId'])
+
+
+    print("After list: ",res_list)
+
+    result = []
+    for id in res_list:
+        itemobj = table.get_item(
+            Key={
+                    'asin': id
+                }
+            )
+        try :
+            result.append(itemobj['Item'])
+        except :
+            errcount = errcount+1
+
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": True
+        },
+        "body": json.dumps(result)
+    }
+
+    return response
