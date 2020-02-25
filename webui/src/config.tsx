ac06788aea6d9a4fa69d1e79de9bfbb8e71e0147
Chuenwatanakul <panyapoc@f018982d6532.ant.amazon.com>
2020-02-25 14:13:09 +0700

    Added Output needed for WebUI Config

--- webui/src/config.tsx
+++ webui/src/config.tsx
@@ -1,14 +1,14 @@
-const apitree = 'https://hpr4qcneol.execute-api.us-east-1.amazonaws.com/Prod';
+const apitree = 'https://hpr4qcneol.execute-api.us-east-1.amazonaws.com/Prod'; // Cloudformation ProdDataEndpoint
 
 export default {
   region: "us-east-1",
   kinesis:{
-    StreamName: "pocstor-Clickstream",
+    StreamName: "pocstor-Clickstream",  //Cloudformation WebUI WebUIStreamName
     PartitionKey: "webpartition"
   },
   cognito: {
-    SignInUrl: "https://all-store.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=2nr6bddje6ekkd93ac79bsd4l2&redirect_uri=http://localhost:3000/login",
-    AnonymousPoolId : "us-east-1:5cd563db-7e9b-44f2-97c6-abfe475be5d4"
+    // SignInUrl: "https://all-store.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=2nr6bddje6ekkd93ac79bsd4l2&redirect_uri=http://localhost:3000/login",
+    AnonymousPoolId : "us-east-1:5cd563db-7e9b-44f2-97c6-abfe475be5d4" // Cloudformation WebUIAnonymousPoolId
 
   },
   api: {
