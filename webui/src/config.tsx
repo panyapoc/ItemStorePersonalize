0ba542142b8ffe2ad17ef46865850f13383fd3ad
Chuenwatanakul <panyapoc@f018982d6532.ant.amazon.com>
2020-02-25 14:22:37 +0700

    Added post step for WebUI

--- webui/src/config.tsx
+++ webui/src/config.tsx
@@ -1,23 +1,24 @@
-const apitree = 'https://hpr4qcneol.execute-api.us-east-1.amazonaws.com/Prod'; // Cloudformation ProdDataEndpoint
+const Apitree = ''; // Cloudformation ProdDataEndpoint
+const AnonymousPoolId = '' //Cloudformation WebUIAnonymousPoolId
+const StreamName = '' // Cloudformation WebUIStreamName
+
 
 export default {
   region: "us-east-1",
   kinesis:{
-    StreamName: "pocstor-Clickstream",  //Cloudformation WebUI WebUIStreamName
+    StreamName: AnonymousPoolId,
     PartitionKey: "webpartition"
   },
   cognito: {
-    // SignInUrl: "https://all-store.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=2nr6bddje6ekkd93ac79bsd4l2&redirect_uri=http://localhost:3000/login",
-    AnonymousPoolId : "us-east-1:5cd563db-7e9b-44f2-97c6-abfe475be5d4" // Cloudformation WebUIAnonymousPoolId
-
+    AnonymousPoolId : StreamName
   },
   api: {
-    GetListUrl: `${apitree}/recommendations/`,
-    GetDetailsUrl: `${apitree}/items/`,
-    ClickEventUrl: `${apitree}/clickevent`,
-    SearchUrl: `${apitree}/search`,
-    RecommendSimilar: `${apitree}/recommendationsitem/`,
-    GetDescriptionForProduct: `${apitree}/description`
+    GetListUrl: `${Apitree}/recommendations/`,
+    GetDetailsUrl: `${Apitree}/items/`,
+    ClickEventUrl: `${Apitree}/clickevent`,
+    SearchUrl: `${Apitree}/search`,
+    RecommendSimilar: `${Apitree}/recommendationsitem/`,
+    GetDescriptionForProduct: `${Apitree}/description`
   },
   user: {
     id: "AIXZKN4ACSKI"
